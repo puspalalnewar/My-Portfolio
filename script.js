a64122c6f48e01d0f520a93e179fa1f44370b914
Puspalal <puspalalnewar79@gmail.com>
2024-02-20 20:08:59 +0530

    Added app file

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 let ham = document.getElementById("class-i");
 let xBar = document.getElementById("class-j");
 let navCont = document.querySelector(".nav-container");
-let b = document.querySelector("body");
-let c = document.querySelector(".nav-links");
+// let b = document.querySelector("body");
+// let c = document.querySelector(".nav-links");
 navCont.style.display = "none";
 
-
-
-if (c.style.display === "block") {
-    b.style.color = "red"; 
-    // ham.style.display = "none";
-    // navCont.style.display = "none";
-    // xBar.style.display = "none";
-}
 ham.addEventListener("click", () => {
     if (navCont.style.display === "none") {
         navCont.style.display = "block";
@@ -26,14 +18,6 @@ xBar.addEventListener("click", () => {
     xBar.style.display = "none";
 });
 
-
-
-
-// if(b.style.width >= "762px"){
-//     ham.style.display = "none";
-//     navCont.style.display = "none";
-//     xBar.style.display = "none";
-// }
 
 
 
